aa6c607a05954c6d24dc0c4e1ae0972cc0893457
Sulthan Qintara <sulthanqintara@gmail.com>
2023-09-04 13:28:28 +0700

    add auth form

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,9 +1,18 @@
+"use client";
+
 import React from "react";
+import AuthForm from "../AuthForm";
 
 export default function Login() {
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
+    console.log("user login", email, password);
+  };
+
   return (
     <main>
       <h2 className="text-center">Login</h2>
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   );
 }
--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,9 +1,17 @@
+"use client"
+
+import AuthForm from "../AuthForm";
+
 export default function Signup() {
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
+    console.log("user signup", email, password);
+  };
+
   return (
     <main>
-      <h2 className="text-center">
-        Sign Up
-      </h2>
+      <h2 className="text-center">Sign Up</h2>
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   );
 }
