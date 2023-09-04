7148f1c0faf5213604e0633babb1db256f2dffbb
Sulthan Qintara <sulthanqintara@gmail.com>
2023-09-04 14:08:51 +0700

    Sign in functionality

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,18 +1,33 @@
 "use client";
 
 import React from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
+// components
 import AuthForm from "../AuthForm";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
+  const [error, setError] = useState("");
+  const router = useRouter();
+
   const handleSubmit = async (e, email, password) => {
     e.preventDefault();
-    console.log("user login", email, password);
+    setError("");
+    const supabase = createClientComponentClient();
+    const { error } = await supabase.auth.signInWithPassword({ email, password });
+    if (error) {
+      return setError(error.message);
+    }
+    return router.push("/dashboard");
   };
 
   return (
     <main>
       <h2 className="text-center">Login</h2>
       <AuthForm handleSubmit={handleSubmit} />
+
+      {error && <div className="error">{error}</div>}
     </main>
   );
 }
