32f22457b45228f60133f0ce1412db065b8c9d25
Sulthan Qintara <sulthanqintara@gmail.com>
2023-09-04 14:04:28 +0700

    Add logout button and functionality

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 import Logo from "./dojo-logo.png";
 import Image from "next/image";
+import LogoutButton from "./LogoutButton";
 
 export default function Navbar({ user }) {
   return (
@@ -9,8 +10,12 @@ export default function Navbar({ user }) {
       <Image src={Logo} alt="Dashboard Logo" width={70} quality={100} placeholder="blur" />
       <h1>Helpdesk</h1>
       <Link href="/">Dashboard</Link>
-      <Link href="/tickets">Tickets</Link>
+      <Link href="/tickets" className="mr-auto">
+        Tickets
+      </Link>
+
       {user && <span>Hello, {user.email}</span>}
+      <LogoutButton />
     </nav>
   );
 }
