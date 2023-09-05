acdf0b2c8ba4ac7faab6c79bcf506487e038d904
Sulthan Qintara <sulthanqintara@gmail.com>
2023-09-05 18:13:01 +0700

    Add cors policy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: `${
+              process.env.NODE_ENV === "development"
+                ? "*"
+                : "https://nextjs-13-three.vercel.app, https://nextjs-13-msulthanq.vercel.app, https://nextjs-13-git-master-msulthanq.vercel.app"
+            }`,
+          }, // replace this your actual origin
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
