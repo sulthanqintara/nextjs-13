f562ad31e2e7ccfa1d284dd3cd2288e36b1825bf
Sulthan Qintara <sulthanqintara@gmail.com>
2023-09-16 22:57:48 +0700

    Wildcard cors

--- next.config.js
+++ next.config.js
@@ -8,8 +8,8 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://nextjs-13-three.vercel.app" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "*" },
           {
             key: "Access-Control-Allow-Headers",
             value:
